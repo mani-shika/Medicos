ebe128f5fd32a1a6f57d7979c5dfc0e3233cf92a
SoleSTAR0007 <bivaspaul.self7@gmail.com>
2026-03-10 02:41:13 +0530

    enabling multiple local host

--- backend/src/main/java/com/medico/backend/config/CorsConfig.java
+++ backend/src/main/java/com/medico/backend/config/CorsConfig.java
@@ -14,7 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // frontend URL
+                        .allowedOrigins(
+                           "http://localhost:3000",
+                           "http://localhost:5500",
+                           "http://127.0.0.1:5500",
+                           "http://localhost:5501",
+                           "http://127.0.0.1:5501"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
